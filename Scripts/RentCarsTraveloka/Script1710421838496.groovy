3184c718ebeb23603ad01879a0443b168ce378db
rosyidridho <rosyidridho@localhost>
2024-03-14 20:10:51 +0700

    test

---
+++ Scripts/RentCarsTraveloka/Script1710421838496.groovy
@@ -0,0 +1,127 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://www.traveloka.com/en-id')
+
+WebUI.maximizeWindow()
+
+WebUI.click(findTestObject('Object Repository/Page_Traveloka - Southeast Asias Leading Tr_13aaf0/div_Car Rental'))
+
+WebUI.click(findTestObject('Object Repository/Page_Traveloka - Southeast Asias Leading Tr_13aaf0/svg_Memverifikasi_Bz112c Bz112c-r9oPif'))
+
+WebUI.setText(findTestObject('Object Repository/Page_Traveloka - Southeast Asias Leading Tr_13aaf0/input_Your Rental Location_css-11aywtz r-cw_d2b2f7'), 
+    'Jakarta')
+
+WebUI.click(findTestObject('Object Repository/Page_Traveloka - Southeast Asias Leading Tr_13aaf0/h3_Jakarta'))
+
+WebUI.click(findTestObject('Object Repository/Page_Traveloka - Southeast Asias Leading Tr_13aaf0/input_Rental Start Date_css-11aywtz r-cwxd7_e52648'))
+
+WebUI.click(findTestObject('Object Repository/Page_Traveloka - Southeast Asias Leading Tr_13aaf0/div_16'))
+
+WebUI.click(findTestObject('Object Repository/Page_Traveloka - Southeast Asias Leading Tr_13aaf0/input_Rental Start Date_css-11aywtz r-cwxd7_e52648'))
+
+WebUI.click(findTestObject('Object Repository/Page_Traveloka - Southeast Asias Leading Tr_13aaf0/div_Done'))
+
+WebUI.click(findTestObject('Object Repository/Page_Traveloka - Southeast Asias Leading Tr_13aaf0/input_Rental Start Date_css-11aywtz r-cwxd7_e52648'))
+
+WebUI.click(findTestObject('Object Repository/Page_Traveloka - Southeast Asias Leading Tr_13aaf0/div_19'))
+
+WebUI.click(findTestObject('Object Repository/Page_Traveloka - Southeast Asias Leading Tr_13aaf0/input_Rental Start Date_css-11aywtz r-cwxd7_e52648'))
+
+WebUI.click(findTestObject('Object Repository/Page_Traveloka - Southeast Asias Leading Tr_13aaf0/div_11'))
+
+WebUI.click(findTestObject('Object Repository/Page_Traveloka - Southeast Asias Leading Tr_13aaf0/div_Done'))
+
+WebUI.click(findTestObject('Object Repository/Page_Traveloka - Southeast Asias Leading Tr_13aaf0/div_End Time_css-18t94o4 css-1dbjc4n r-kdyh_855d06'))
+
+WebUI.click(findTestObject('Object Repository/Page_Cheapest Car Rental from Rp200kday Wit_6af24f/div_Continue'))
+
+WebUI.click(findTestObject('Object Repository/Page_Cheapest Car Rental from Rp200kday Wit_6af24f/div_Continue'))
+
+WebUI.click(findTestObject('Object Repository/Page_Cheapest Car Rental from Rp200kday Wit_6af24f/div_Continue'))
+
+WebUI.click(findTestObject('Object Repository/Page_Cheapest Car Rental from Rp200kday Wit_6af24f/div_Please Select Below Options_css-1dbjc4n_0314af'))
+
+WebUI.click(findTestObject('Object Repository/Page_Cheapest Car Rental from Rp200kday Wit_6af24f/div_Rental Office'))
+
+WebUI.click(findTestObject('Object Repository/Page_Cheapest Car Rental from Rp200kday Wit_6af24f/div_Rental Office_css-1dbjc4n r-1q9bdsx r-d_1a72b8'))
+
+WebUI.click(findTestObject('Object Repository/Page_Cheapest Car Rental from Rp200kday Wit_6af24f/div_Please Select Below Options_css-1dbjc4n_0314af_1'))
+
+WebUI.click(findTestObject('Object Repository/Page_Cheapest Car Rental from Rp200kday Wit_6af24f/input_Search location or address_css-11aywt_c3c58f'))
+
+WebUI.click(findTestObject('Object Repository/Page_Cheapest Car Rental from Rp200kday Wit_6af24f/h3_Gambir Station'))
+
+WebUI.click(findTestObject('Object Repository/Page_Cheapest Car Rental from Rp200kday Wit_6af24f/div_Continue'))
+
+WebUI.click(findTestObject('Object Repository/Page_Traveloka - Southeast Asias Leading Tr_13aaf0/awswaf-captcha'))
+
+WebUI.click(findTestObject('Object Repository/Page_Traveloka - Southeast Asias Leading Tr_13aaf0/awswaf-captcha'))
+
+WebUI.click(findTestObject('Object Repository/Page_Traveloka - Southeast Asias Leading Tr_13aaf0/awswaf-captcha'))
+
+WebUI.click(findTestObject('Object Repository/Page_Traveloka - Southeast Asias Leading Tr_13aaf0/awswaf-captcha'))
+
+WebUI.click(findTestObject('Object Repository/Page_Traveloka - Southeast Asias Leading Tr_13aaf0/awswaf-captcha'))
+
+WebUI.setText(findTestObject('Object Repository/Page_Traveloka - Southeast Asias Leading Tr_13aaf0/input_Rental Start Date_css-11aywtz r-cwxd7_e52648'), 
+    'Rosyid Ridho')
+
+WebUI.setText(findTestObject('Object Repository/Page_Traveloka - Southeast Asias Leading Tr_13aaf0/input__css-11aywtz r-13awgt0 r-t1w4ow r-ube_1d9001'), 
+    '81314451830')
+
+WebUI.setText(findTestObject('Object Repository/Page_Traveloka - Southeast Asias Leading Tr_13aaf0/input_Rental Start Date_css-11aywtz r-cwxd7_e52648'), 
+    'rosyidridho49@gmail.com')
+
+WebUI.selectOptionByValue(findTestObject('Object Repository/Page_Traveloka - Southeast Asias Leading Tr_13aaf0/select_Mr.Mrs.Ms'), 
+    'MR', true)
+
+WebUI.setText(findTestObject('Object Repository/Page_Traveloka - Southeast Asias Leading Tr_13aaf0/input_Rental Start Date_css-11aywtz r-cwxd7_e52648'), 
+    'Rosyid')
+
+WebUI.setText(findTestObject('Object Repository/Page_Traveloka - Southeast Asias Leading Tr_13aaf0/input__css-11aywtz r-13awgt0 r-t1w4ow r-ube_1d9001'), 
+    '81314451830')
+
+WebUI.click(findTestObject('Object Repository/Page_Traveloka - Southeast Asias Leading Tr_13aaf0/div_ContinueContinue'))
+
+WebUI.setText(findTestObject('Object Repository/Page_Traveloka - Southeast Asias Leading Tr_13aaf0/textarea_mobil wangi'), 
+    'mobil wangi')
+
+WebUI.click(findTestObject('Object Repository/Page_Traveloka - Southeast Asias Leading Tr_13aaf0/div_Tap to check the requirements'))
+
+WebUI.click(findTestObject('Object Repository/Page_Traveloka - Southeast Asias Leading Tr_13aaf0/div_Check All'))
+
+WebUI.click(findTestObject('Object Repository/Page_Traveloka - Southeast Asias Leading Tr_13aaf0/div_Save'))
+
+WebUI.click(findTestObject('Object Repository/Page_Traveloka - Southeast Asias Leading Tr_13aaf0/div_Continue to Payment'))
+
+WebUI.click(findTestObject('Object Repository/Page_Traveloka - Southeast Asias Leading Tr_13aaf0/div_Continue'))
+
+WebUI.click(findTestObject('Object Repository/Page_Cheap Flights Tickets Traveloka - Onli_2df141/div_Bank Transfer'))
+
+WebUI.click(findTestObject('Object Repository/Page_Cheap Flights Tickets Traveloka - Onli_2df141/div_Indomaret'))
+
+WebUI.click(findTestObject('Object Repository/Page_Cheap Flights Tickets Traveloka - Onli_2df141/div_Pay with Indomaret'))
+
+WebUI.delay(5)
+
+WebUI.closeBrowser()
+
